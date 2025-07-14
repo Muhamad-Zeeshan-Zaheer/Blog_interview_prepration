de99a48278bdb139d7d5681f77ef8bd6487cd859
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-07-14 05:26:18 +0500

    Added more practice

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  get "/articles", to: "articles#index"
+  # routes for articles crud operation
+  resources :articles
 end

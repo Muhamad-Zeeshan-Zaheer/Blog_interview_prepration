b42a02f74ccfe7e4a7c63d9aba199b833500b156
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-07-16 22:03:47 +0500

    more practice added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # routes for articles crud operation
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
